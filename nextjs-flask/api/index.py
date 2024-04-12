6dd37fc48813ef03ecb1e492aeedd017450cdedd
Zachary Laguna <lagunazachary@gmail.com>
2024-04-12 14:56:12 +0000

    implement speech to text

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/interview/send_message")
+def interview():
+    return "<p>this is a response</p>"
