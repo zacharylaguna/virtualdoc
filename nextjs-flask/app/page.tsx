729da961d66be132b28bfa7a46c9ed50985023cb
Zachary Laguna <lagunazachary@gmail.com>
2024-06-24 01:08:23 +0000

    add style changes

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -1,116 +1,55 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import '../styles/homepage.css';
+import '../styles/webflow.css';
+import '../styles/normalize.css';
+import Script from 'next/script';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
-          </Link>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    <body>
+      <div className="bars-wrapper w-clearfix">
+        <div className="bar"></div>
+        <div className="bar _2"></div>
+        <div className="bar _3"></div>
+        <div className="bar _4"></div>
+        <div className="bar _5"></div>
+        <div className="bar _6"></div>
+        <div className="bar"></div>
+      </div>
+      <div className="header-section">
+        <div className="container w-container">
+          <h1>VirtualDoc</h1>
+          <p className="subtitle">Tell us your symptoms and receive solutions instantly. Get diagnosed today!</p>
+          <a href="interact" className="button w-button">Start Chat</a>
+          <div className="image-crop"><img src="images/360_F_552695735_INapeIqxVtUtmGRbrbX8KpNafdJJ65uh-removebg-preview.png" sizes="(max-width: 479px) 96vw, (max-width: 590px) 87vw, 514px" srcSet="images/360_F_552695735_INapeIqxVtUtmGRbrbX8KpNafdJJ65uh-removebg-preview-p-500.png 500w, images/360_F_552695735_INapeIqxVtUtmGRbrbX8KpNafdJJ65uh-removebg-preview.png 514w" alt="" /></div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="social-section">
+        <div className="w-container">
+          <h2>A disclaimer about diagnoses...</h2>
+          <div className="refer">* Diagnoses are computer generated recommendations and are not medical advice. We always recommend you see a medical professional for any ailments you may have</div>
+          <div className="share-wrapper"></div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="footer-section">
+        <div className="w-container">
+          <div className="w-row">
+            <div className="w-col w-col-6 w-col-small-6">
+              <div className="copyright">Created by Zachary Laguna</div>
+            </div>
+            <div className="align-right w-col w-col-6 w-col-small-6">
+              <a href="http://facebook.com/webflowapp" className="social-btn w-inline-block"><img src="images/facebook-icon.svg" alt=""></img></a>
+              <a href="http://twitter.com/webflowapp" className="social-btn w-inline-block"><img src="images/twitter-icon.svg" alt=""></img></a>
+              <a href="mailto:support@webflow.com" className="social-btn w-inline-block"><img src="images/email-icon.svg" alt=""></img></a>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+      <Script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66728a50b80bbf38b03fafac" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossOrigin="anonymous"></Script>
+      <script src="js/webflow.js" type="text/javascript"></script>
+    </body>
   )
 }
